a6583ebae2a46f79fa6017cb79374803878ef6b6
Mathieu Dupont <mathieu.jean.dupont@gmail.com>
2026-01-02 16:21:59 -0500

    Update docusaurus.config.js

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -9,158 +9,158 @@ const darkCodeTheme = themes.vsDark;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: siteConfig.nom,
-  tagline: siteConfig.description,
-  url: "https://info.cegepmontpetit.ca/",
-  baseUrl: `/${siteConfig.nomUrl}/`,
-  onBrokenLinks: "throw",
-  favicon: "img/favicon.ico",
+    title: siteConfig.nom,
+    tagline: siteConfig.description,
+    url: "https://info.cegepmontpetit.ca/",
+    baseUrl: `/${siteConfig.nomUrl}/`,
+    onBrokenLinks: "throw",
+    favicon: "img/favicon.ico",
 
-  organizationName: "departement-info-cem",
-  projectName: siteConfig.nomUrl,
-  deploymentBranch: "gh-pages",
-  trailingSlash: false,
+    organizationName: "departement-info-cem",
+    projectName: siteConfig.nomUrl,
+    deploymentBranch: "gh-pages",
+    trailingSlash: false,
 
-  i18n: {
-    defaultLocale: "fr",
-    locales: ["fr"],
-  },
-
-  markdown: {
-    mermaid: true,
-    hooks: {
-      onBrokenMarkdownLinks: "warn",
+    i18n: {
+        defaultLocale: "fr",
+        locales: ["fr"],
     },
-  },
 
-  themes: ["@docusaurus/theme-mermaid"],
+    markdown: {
+        mermaid: true,
+        hooks: {
+            onBrokenMarkdownLinks: "warn",
+        },
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          routeBasePath: "/",
-          editUrl: `https://github.com/departement-info-cem/${siteConfig.nomUrl}/tree/main/web`,
-        },
-        theme: {
-          customCss: [require.resolve("./src/css/custom.css")],
-        },
-      }),
+    themes: ["@docusaurus/theme-mermaid"],
+
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    routeBasePath: "/",
+                    editUrl: `https://github.com/departement-info-cem/${siteConfig.nomUrl}/tree/main/web`,
+                },
+                theme: {
+                    customCss: [require.resolve("./src/css/custom.css")],
+                },
+            }),
+        ],
     ],
-  ],
 
-  plugins: [require.resolve("./plugins/docs-metadata")],
+    plugins: [require.resolve("./plugins/docs-metadata")],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      docs: {
-        sidebar: {
-          hideable: true,
-        },
-      },
-        colorMode: {
-          defaultMode: 'dark',               // 🌙 site démarre en mode sombre
-          respectPrefersColorScheme: false,  // ignore le mode du système
-        },
-      navbar: {
-        title: siteConfig.nom,
-        logo: {
-          alt: "Logo CEM",
-          src: "img/logo.svg",
-        },
-        items: [
-          {
-            type: "doc",
-            docId: "cours/semaine1",
-            position: "left",
-            label: "Cours",
-          },
-          {
-            type: "docSidebar",
-            position: "left",
-            sidebarId: "projets",
-            label: "Projets",
-          },
-          {
-            label: "SN1 (Programmation)",
-            href: `https://info.cegepmontpetit.ca/420-SN1/`,
-            position: "right",
-          },
-          {
-            label: "4A4 (Réseaux de neurones)",
-            href: `https://info.cegepmontpetit.ca/420-4A4/`,
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Sources",
-            items: [
-              {
-                label: "GitHub",
-                href: `https://github.com/departement-info-cem/${siteConfig.nomUrl}`,
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()}. ${
-          siteConfig.nom
-        }. CÉGEP Édouard-Montpetit.`,
-      },
-      // Décommenter et remplir pour activer l'indexation des pages par le moteur de recherche local
-      // algolia: {
-      //   appId: '',
-      //   apiKey: '',
-      //   indexName: '',
-      //   contextualSearch: true,
-      //   searchPagePath: 'search',
-      // },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ["csharp", "java", "dart"],
-      },
-      metadata: [
-        {
-          name: "keywords",
-          content: `${siteConfig.nom}, ${siteConfig.description},
+        ({
+            docs: {
+                sidebar: {
+                    hideable: true,
+                },
+            },
+            colorMode: {
+                defaultMode: 'dark',               // 🌙 site démarre en mode sombre
+                respectPrefersColorScheme: false,  // ignore le mode du système
+            },
+            navbar: {
+                title: siteConfig.nom,
+                logo: {
+                    alt: "Logo CEM",
+                    src: "img/logo.svg",
+                },
+                items: [
+                    {
+                        type: "doc",
+                        docId: "cours/semaine1",
+                        position: "left",
+                        label: "Cours",
+                    },
+                    {
+                        type: "docSidebar",
+                        position: "left",
+                        sidebarId: "projets",
+                        label: "Projets",
+                    },
+                    {
+                        label: "SN1 (Programmation)",
+                        href: `https://info.cegepmontpetit.ca/420-SN1/`,
+                        position: "right",
+                    },
+                    {
+                        label: "4A4 (Réseaux de neurones)",
+                        href: `https://info.cegepmontpetit.ca/420-4A4/`,
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Sources",
+                        items: [
+                            {
+                                label: "GitHub",
+                                href: `https://github.com/departement-info-cem/${siteConfig.nomUrl}`,
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()}. ${
+                    siteConfig.nom
+                }. CÉGEP Édouard-Montpetit.`,
+            },
+            // Décommenter et remplir pour activer l'indexation des pages par le moteur de recherche local
+            // algolia: {
+            //   appId: '',
+            //   apiKey: '',
+            //   indexName: '',
+            //   contextualSearch: true,
+            //   searchPagePath: 'search',
+            // },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+                additionalLanguages: ["csharp", "java", "dart"],
+            },
+            metadata: [
+                {
+                    name: "keywords",
+                    content: `${siteConfig.nom}, ${siteConfig.description},
             4A3, 360-4A3, cem, sciences de la nature, projet d'intégration, projet integration, programmation,
             python, cours programmation cégep, cégep Édouard-Montpetit, cegep Edouard Montpetit,
             informatique, cégep, cegep, édouard-montpetit, edouard-montpetit, édouard montpetit, edouard montpetit`,
-        },
-        {
-            name: "description",
-            content: siteConfig.description,
-        },
-        {
-            property: "og:title",
-            content: siteConfig.nom,
-        },
-        {
-            property: "og:description",
-            content: siteConfig.description,
-        },
-        {
-            property: "og:type",
-            content: "website",
-        },
-        {
-            property: "og:image",
-            content: "https://info.cegepmontpetit.ca/360-4A3/img/logo.svg"
-        },
-        {
-            property: "og:url",
-            content: "https://info.cegepmontpetit.ca/360-4A3/"
-        }
-      ],
-    }),
+                },
+                {
+                    name: "description",
+                    content: siteConfig.description,
+                },
+                {
+                    property: "og:title",
+                    content: siteConfig.nom,
+                },
+                {
+                    property: "og:description",
+                    content: siteConfig.description,
+                },
+                {
+                    property: "og:type",
+                    content: "website",
+                },
+                {
+                    property: "og:image",
+                    content: "https://info.cegepmontpetit.ca/360-4A3/img/logo.svg"
+                },
+                {
+                    property: "og:url",
+                    content: "https://info.cegepmontpetit.ca/360-4A3/"
+                }
+            ],
+        }),
 };
 
 module.exports = config;
